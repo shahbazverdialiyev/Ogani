3e17dbf4e6b56aeefa89a4c0983648645afa4d23
Shahbaz <verdialiyevsahbaz@gmail.com>
2025-04-15 22:52:43 +0400

    Add some Validators

---
+++ Ogani.WebApp/Ogani.WebApp.Business/Validators/CategoryValidators/CategoryCreateValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Ogani.WebApp.DTOs.CategoryDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogani.WebApp.Business.Validators.CategoryValidators
+{
+    internal class CategoryCreateValidator : AbstractValidator<CategoryCreateDTO>
+    {
+        public CategoryCreateValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .Length(2, 50).WithMessage("Name must be between 2 and 50 characters.");
+            RuleFor(x => x.Description)
+                .Length(10, 200).WithMessage("Description must be between 10 and 200 characters.");
+        }
+    }
+}
---
+++ Ogani.WebApp/Ogani.WebApp.Business/Validators/CategoryValidators/CategoryUpdateValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Ogani.WebApp.DTOs.CategoryDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogani.WebApp.Business.Validators.CategoryValidators
+{
+    public class CategoryUpdateValidator : AbstractValidator<CategoryUpdateDTO>
+    {
+        public CategoryUpdateValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage("Id is required.")
+                .GreaterThan(0).WithMessage("Id must be greater than 0.");
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .Length(2, 50).WithMessage("Name must be between 2 and 50 characters.");
+            RuleFor(x => x.Description)
+                .Length(10, 200).WithMessage("Description must be between 10 and 200 characters.");
+        }
+    }
+}
