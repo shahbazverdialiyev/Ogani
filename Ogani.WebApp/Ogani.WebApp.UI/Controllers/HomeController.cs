8d7c95e717021bf4f6a73ec7a976d732482a1cbc
Shahbaz <verdialiyevsahbaz@gmail.com>
2025-04-21 23:55:11 +0400

    add razor pages to UI

---
+++ Ogani.WebApp/Ogani.WebApp.UI/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ogani.WebApp.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+        }
+    }
+}
